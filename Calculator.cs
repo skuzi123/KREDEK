488aa368a3cd71dddb6a82112616a480125b4a15
skuzi123 <mateuszskuza10@spoko.pl>
2021-12-03 14:00:10 +0100

    zajecia 1

---
+++ Calculator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pracadomowa1
+{
+    class Calculator
+    {
+        private double a;
+        private double b;
+        public Calculator()
+        {
+           
+        }
+       
+        public void EnterVariables()
+        {
+            Console.WriteLine("Enter a: ");
+            a= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+        }
+        public void Addiction()
+        {
+
+            Console.WriteLine(a+b);
+        }
+        public void Subtraction()
+        {
+
+            Console.WriteLine(a - b);
+        }
+        public void Multiplication()
+        {
+            Console.WriteLine(a - b);
+        }
+        public void Division()
+        {
+            if (b == 0)
+            {
+               string x="DON'T DIVIDE BY ZERO!!!";
+                Console.WriteLine( x);
+            }
+            else
+            {
+                Console.WriteLine(a / b);
+            }
+             
+        }
+        public void Application()
+        {
+         
+            Console.WriteLine("1.Addiction");
+            Console.WriteLine("2.Subtraction");
+            Console.WriteLine("3.Multiplication");
+            Console.WriteLine("4.Division");
+            Console.WriteLine("Enter choice");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            EnterVariables();
+            switch (choice)
+            {
+                
+                case 1:
+                    Addiction();
+                    break;
+                case 2:
+                    Subtraction();
+                    break;
+                case 3:
+                    Multiplication();
+                    break;
+                case 4:
+                    Division();
+                    break;
+                default:
+                    Console.WriteLine("EXIT");
+                   
+                    break;
+            }
+           
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace pracadomowa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PROGRAM 1 ");
+
+            Console.WriteLine("Enter string: ");
+            string text = Console.ReadLine();
+            Console.WriteLine("Enter char");
+            char sign= Console.ReadLine()[0];
+            Console.WriteLine("Enter int");
+            int number1= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter float");
+            float number2 = Single.Parse(Console.ReadLine());
+            Console.WriteLine("Enter double");
+            double number3= Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(text);
+            Console.WriteLine(sign);
+            Console.WriteLine(number1);
+            Console.WriteLine(number2);
+            Console.WriteLine(number3);
+
+            Console.WriteLine("KALKULATOR");
+            Calculator calculator = new Calculator();
+            calculator.Application();
+
+
+
+
+
+
+        }
+    }
+}
